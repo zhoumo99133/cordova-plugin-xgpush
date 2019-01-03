20f3496b5ed98da10f03e7570b6501e62f6309fd
dishuostec <dishuostec@gmail.com>
2019-01-03 17:44:12 +0800

    添加推送注册状态提示

--- src/ios/AppDelegate+CDVXGPush.m
+++ src/ios/AppDelegate+CDVXGPush.m
@@ -8,14 +8,14 @@
 
 
 +(void)load{
-    
+
     Method origin = class_getInstanceMethod([self class],@selector(init));
     Method swizzle =class_getInstanceMethod([self class], @selector(init_plus));
     method_exchangeImplementations(origin, swizzle);
 }
 
 -(instancetype)init_plus{
-    
+
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(applicationDidLaunch:)
                                                  name:UIApplicationDidFinishLaunchingNotification
@@ -24,7 +24,7 @@
 }
 
 -(void)applicationDidLaunch:(NSNotification *)notification{
-    
+
     if (notification) {
         [CDVXGPushPlugin setLaunchOptions:notification.userInfo];
     }
@@ -39,17 +39,35 @@
 
 
 -(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
-    
+
     NSLog(@"[AppDelegate] receive local notification");
-    
+
     //notification是发送推送时传入的字典信息
 //    [XGPush localNotificationAtFrontEnd:notification userInfoKey:@"clockID" userInfoValue:@"myid"];
-    
+
     //删除推送列表中的这一条
 //    [XGPush delLocalNotification:notification];
-    
+
     //清空推送列表
     //[XGPush clearLocalNotifications];
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSString * deviceTokenStr = [[XGPushTokenManager defaultTokenManager] deviceTokenString];
+
+#ifdef DEBUG
+    NSLog(@"[XGPushPlugin] device token is %@",deviceTokenStr);
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"推送注册失败" message:deviceTokenStr delegate:self cancelButtonTitle:nil otherButtonTitles:@"ok", nil];
+    [alert show];
+#endif
+}
+
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"[XGPushPlugin] 推送注册失败：%@", error);
+
+    NSString *errorInfo = [NSString stringWithFormat:@"%@",error.localizedDescription];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"推送注册失败" message:errorInfo delegate:self cancelButtonTitle:nil otherButtonTitles:@"ok", nil];
+    [alert show];
+}
 @end
